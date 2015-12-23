ce1bd9935aa9a08d67dc82971ff818c219affda3
Abhi Agarwal <me@abhiagarwal.com>
2015-12-23 15:13:37 -0500

    Adding sample react-native buttons

--- app/index.ios.js
+++ app/index.ios.js
@@ -10,33 +10,53 @@ var {
   StyleSheet,
   Text,
   View,
+  ScrollView,
+  NavigatorIOS,
+  TouchableOpacity,
 } = React;
 
-var contextapp = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+var Buttons = require('./context/buttons');
+
+var Home = React.createClass({
+  render: function () {
+   return (
+      <ScrollView style={styles.list}
+                  contentContainerStyle={styles.container}>
+        <TouchableOpacity onPress={() => {
+          this.props.navigator.push({
+            title: 'Buttons',
+            component: Buttons,
+          });
+        }}>
+          <Text style={styles.pushLabel}>Buttons</Text>
+        </TouchableOpacity>
+      </ScrollView>
     );
-  }
+}
+});
+
+var contextapp = React.createClass({
+  render: function () {
+    return (
+      <NavigatorIOS
+        style={{flex:1}}
+        initialRoute={{
+          component: Home,
+          title: 'Context App',
+        }}
+      />
+    );
+  },
 });
 
 var styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#F5FCFF',
+    paddingTop: 20,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
@@ -46,8 +66,12 @@ var styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginTop: 20, marginBottom: 0,
   },
+  pushLabel: {
+    padding: 10,
+    color: '#2196F3',
+  }
 });
 
 AppRegistry.registerComponent('contextapp', () => contextapp);
